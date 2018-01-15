e77d73c205850ae2cb37b7f90eb8baad0be2f7c5
Hlynur Stefansson <hlynurs15@ru.is>
2018-01-15 13:58:08 +0000

    Add DatabaseController Script

---
+++ Assets/DatabaseControllerScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+
+public class DatabaseControllerScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// Set this before calling into the realtime database.
+    	FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://skriduklaustur-unity.firebaseio.com/");
+
+		// Get the root reference location of the database.
+    	DatabaseReference reference = FirebaseDatabase.DefaultInstance.RootReference;
+
+
+		// TESTING FIREBASE STUFF
+		FirebaseDatabase.DefaultInstance
+		.GetReference("challenges")
+		.GetValueAsync().ContinueWith(task => {
+			if (task.IsFaulted) {
+				// Handle the error...
+				Debug.Log("Failed to get information from Firebase");
+			}
+			else if (task.IsCompleted) {
+				DataSnapshot snapshot = task.Result;
+				// Do something with snapshot...
+				Debug.Log(snapshot.Children);
+			}
+		});
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
